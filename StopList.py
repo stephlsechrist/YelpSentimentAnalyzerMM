e1444bebb4fa6b8f015a678d5b6735e64f734d99
Stephanie Sechrist <stephlsechrist@gmail.com>
2020-04-10 01:43:28 -0700

    alphabetized stop-list to make adding entries easier

--- StopList.py
+++ StopList.py
@@ -1,14 +1,12 @@
 
 class StopList:
     def __init__(self):
-        self.list = ['to', 'and', 'the', 'i', 'for', 'a', 'in', 'he', 'she',
-                     'have', 'but', 'this', 'next', 'my', 'into', 'was', 'after',
-                     'few', 'place', 'had', 'start', 'up', 'today', 'decided',
-                     'door', 'spot', 'ordered', 'pack', 'some', 'go', 'it', 'ready',
-                     'than', 'minutes', 'all', 'that', 'you', 'do', 'keep', 'cant',
-                     'till', 'your', 'been', 'of', 'with', 'item', 'day', 'night',
-                     'item', 'weeks', 'items', 'first', 'roll', 'biscuits', 'chicken',
-                     'egg', 'cheese', 'gravy', 'wait']
-
-
-
+        self.list = ['a', 'after', 'all', 'and', 'been', 'biscuits', 'but',
+                     'cant', 'cheese', 'chicken', 'day', 'decided', 'do',
+                     'door', 'egg', 'few', 'first', 'for', 'from', 'go',
+                     'gravy', 'had', 'have', 'he', 'i', 'if', 'in', 'into',
+                     'it', 'item', 'item', 'items', 'keep', 'minutes', 'my',
+                     'next', 'night', 'of', 'ordered', 'pack', 'place', 'ready',
+                     'roll', 'she', 'some', 'spot', 'start', 'than', 'that', 'the',
+                     'this', 'til', 'till', 'to', 'to', 'today', 'up', 'wait', 'was',
+                     'weeks', 'when', 'where', 'with', 'you', 'your']
