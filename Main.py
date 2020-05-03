87431fe5e7f5c5c2b300873149bf03f1ba0739ea
Tom Sechrist <tsechrist@mail.sfsu.edu>
2020-05-03 14:41:53 -0700

    cleaning up main

--- Main.py
+++ Main.py
@@ -3,12 +3,10 @@ from TextParser import TextParser
 from TermFrequencyTable import TermFrequencyTable
 from SampleReviews import SampleReviews
 
-sample = SampleReviews()
 tft = TermFrequencyTable()
 
-for review in sample.listOfReviews:
-    parsedReview = TextParser(review)
-    tft.addList(parsedReview.sortByAlpha())
+for review in SampleReviews().listOfReviews:
+    tft.addList(TextParser(review).sortByAlpha())
 
 tft.formatTable()
 tft.displayVert()
