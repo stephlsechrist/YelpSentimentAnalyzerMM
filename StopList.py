4a32ea8386e134e0bae4ea04e53f52b131899009
Stephanie Sechrist <stephlsechrist@gmail.com>
2020-05-02 19:20:16 -0700

    added more 5 star restaurant reviews and updated the stop list

--- StopList.py
+++ StopList.py
@@ -2,15 +2,14 @@ import Stem
 
 class StopList:
     def __init__(self):
-        self.list = Stem.stem(['a', 'after', 'all', 'and', 'at', 'be', 'been', 'biscuits', 'but',
-                     'cant', 'cheese', 'chicken', 'day', 'decided', 'do',
-                     'door', 'egg', 'few', 'first', 'for', 'from', 'go',
-                     'gravy', 'had', 'have', 'he', 'i', 'if', 'in', 'into',
-                     'it', 'item', 'items', 'keep', 'minutes', 'my',
+        self.list = Stem.stem(['a', 'after', 'all', 'and', 'are', 'at', 'be', 'been', 'biscuits', 'but',
+                     'cant', 'cheese', 'chicken', 'city', 'day', 'decided', 'do',
+                     'door', 'eat', 'egg', 'few', 'first', 'food', 'for', 'francisco', 'from', 'get', 'go',
+                     'gravy', 'had', 'have', 'he', 'here', 'i', 'if', 'in', 'into',
+                     'is', 'ive', 'it', 'item', 'items', 'keep', 'menu', 'minutes', 'my',
                      'next', 'night', 'of', 'ordered', 'pack', 'place', 'ready',
-                     'roll', 'she', 'some', 'spot', 'start', 'than', 'that', 'the',
-                     'this', 'til', 'till', 'treats', 'to', 'to', 'today', 'up', 'wait', 'was',
-                     'weeks', 'when', 'where', 'with', 'you', 'your'])
+                     'roll', 'san', 'sf', 'she', 'some', 'spot', 'start', 'than', 'that', 'the',
+                     'this', 'til', 'till', 'treats', 'to', 'today', 'up', 'wait', 'was', 'we',
+                     'weeks', 'what', 'when', 'where', 'who', 'with', 'write', 'you', 'your'])
 
         #maybe add:
-        # 'menu', 'is', ive', 'san', 'francisco', 'city'
