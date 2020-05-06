b145f62f0a3d1020896980e2442d8b702bbe9320
Thomas Sechrist <tsechrist@mail.sfsu.edu>
2020-05-05 18:05:20 -0700

    got it to work with the large set

--- Main.py
+++ Main.py
@@ -2,15 +2,28 @@
 from TextParser import TextParser
 from TermFrequencyTable import TermFrequencyTable
 from SampleReviews import SampleReviews
+import json
+import ast
 
 tft = TermFrequencyTable()
-sample = SampleReviews()
+# sample = SampleReviews()
 
-for review in SampleReviews().listOfReviews:
+review_list = []
+
+# with open('yelp_academic_dataset_business.json', 'r', encoding="utf8") as f:
+#     data = json.load(f)
+# print(data)
+
+data = [json.loads(line) for line in open('yelp_academic_dataset_review.json', 'r', encoding="utf8")]
+
+for i in range(50):
+    if data[i]['stars'] < 2:
+        review_list.append(data[i]['text'])
+
+for review in review_list:
     tft.addList(TextParser(review).sortByAlphaNGram(2))
 
 tft.formatTable()
-
 # tft.displayTermList()
 # tft.displayList()
-# tft.displayVert()
+tft.displayVert()
--- TermFrequencyTable.py
+++ TermFrequencyTable.py
@@ -62,38 +62,32 @@ class TermFrequencyTable:
             total = 0
             tempList = []
             tempList.append(term)
-            if len(term) <= 2:
-                tempList.append("\t\t\t\t\t\t\t\t")
-            elif len(term) <= 6 and len(term) >= 3:
-                tempList.append("\t\t\t\t\t\t\t")
-            elif len(term) >= 7 and len(term) <= 10:
-                tempList.append("\t\t\t\t\t\t")
-            elif len(term) >= 11 and len(term) <= 14:
-                tempList.append("\t\t\t\t\t")
-            elif len(term) >= 15 and len(term) <= 18:
-                tempList.append("\t\t\t\t")
-            elif len(term) >= 19 and len(term) <= 22:
-                tempList.append("\t\t\t")
-            elif len(term) >= 23 and len(term) <= 26:
-                tempList.append("\t\t")
-            else:
-                tempList.append("\t")
+            # if len(term) <= 2:
+            #     tempList.append("\t\t\t\t\t\t\t\t")
+            # elif len(term) <= 6 and len(term) >= 3:
+            #     tempList.append("\t\t\t\t\t\t\t")
+            # elif len(term) >= 7 and len(term) <= 10:
+            #     tempList.append("\t\t\t\t\t\t")
+            # elif len(term) >= 11 and len(term) <= 14:
+            #     tempList.append("\t\t\t\t\t")
+            # elif len(term) >= 15 and len(term) <= 18:
+            #     tempList.append("\t\t\t\t")
+            # elif len(term) >= 19 and len(term) <= 22:
+            #     tempList.append("\t\t\t")
+            # elif len(term) >= 23 and len(term) <= 26:
+            #     tempList.append("\t\t")
+            # else:
+            #     tempList.append("\t")
 
             for listNum in self.termFreqTable:
                 tempList.append(listNum[count])
                 total += int(listNum[count])
             count += 1
 
-            if total > 1:
+            if total > 2:
                 print(tempList[0], end='')
                 # for num in tempList:
                 #     print(num, end='')
                 #     print(" ", end='')
                 print("\t\ttotal: " + str(total))
 
-
-    # def __str__(self):
-    #     return "<Text: %s>" % self.getText()
-
-    # def __repr__(self):
-    #     return str(self)
